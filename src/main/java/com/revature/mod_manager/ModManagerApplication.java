b22fe3e0f5aba9a458b7b0bad707a24df5b19932
Maxwell Moord <maxwellmoord@gmail.com>
2022-07-19 16:48:20 -0400

    Created the initial Spring Boot Layout

---
+++ src/main/java/com/revature/mod_manager/ModManagerApplication.java
@@ -0,0 +1,13 @@
+package com.revature.mod_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/mod_manager/ModManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.mod_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
